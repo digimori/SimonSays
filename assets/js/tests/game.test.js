f84263bcfcbda32e3e557054638e6492f652337f
digimori <hanaorikuu@gmail.com>
2022-12-02 21:03:58 +0000

    base game object keys tested

--- assets/js/tests/game.test.js
+++ assets/js/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game } = require("../game");
+const { game } = require("../game");
 
 
  beforeAll(() => {
@@ -17,4 +17,16 @@
      test("score key exists", () => {
          expect("score" in game).toBe(true);
      });
+     test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
     });
